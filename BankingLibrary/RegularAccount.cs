e735e704ea0eae4c0d4c488b24bbb4d47afd2a38
WimHambrouck <wim.hambrouck@gmail.com>
2020-10-05 16:10:13 +0200

    RegularAccount en SavingsAccount toegevoegd

---
+++ BankingLibrary/RegularAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingLibrary
+{
+    class RegularAccount : Account
+    {
+        public List<string> CreditCards { get; set; }
+
+        public RegularAccount(string iban, decimal balance, DateTime creationDate, decimal interest, List<string> creditCards = null) : base(iban, balance, creationDate, interest)
+        {
+            if(creditCards == null)
+            {
+                CreditCards = new List<string>();
+            }
+            else
+            {
+                CreditCards = creditCards;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + Environment.NewLine +
+                "Credit cards: " + string.Join(", ", CreditCards);
+        }
+    }
+}
---
+++ BankingLibrary/SavingsAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingLibrary
+{
+    class SavingsAccount : Account
+    {
+        public decimal LoyaltyBonus { get; set; }
+
+        public SavingsAccount(string iban, decimal balance, DateTime creationDate, decimal interest, decimal loyaltyBonus) : base(iban, balance, creationDate, interest)
+        {
+            LoyaltyBonus = loyaltyBonus;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Loyalty bonus: {LoyaltyBonus:C}";
+        }
+    }
+}
