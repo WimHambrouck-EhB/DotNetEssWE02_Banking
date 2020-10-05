f3f81f52f8eeb3ecd0f4f7e8fbc8ce28fed83c75
WimHambrouck <wim.hambrouck@gmail.com>
2020-10-05 16:49:03 +0200

    rename projectis

--- BankingWE02/Program.cs
+++ BankingWE02/Program.cs
@@ -8,16 +8,22 @@ namespace BankingWE02
     {
         static void Main(string[] args)
         {
+            var klanten = new List<Client>();
+            klanten.Add(new Client("Wim", "Hambrouck"));
+            klanten.Add(new Client("Johan", "Jannsens"));
+            klanten.Add(new Client("Els", "Leys"));
+
+
 
             var rekeningen = new List<Account>();
-            rekeningen.Add(new RegularAccount("23456", 1000M, DateTime.Now, 0.02M));
-            rekeningen.Add(new SavingsAccount("321654", 2000M, DateTime.Now, 0.05M, 0.02M));
+            rekeningen.Add(new RegularAccount("23456", 1000M, DateTime.Now, 0.02M, klanten[0]));
+            rekeningen.Add(new SavingsAccount("321654", 2000M, DateTime.Now, 0.05M, klanten[2], 0.02M));
 
 
             foreach (var rekening in rekeningen)
             {
                 Console.WriteLine(rekening);
-                Console.WriteLine();
+                Console.WriteLine("----------------------------------------");
             }
 
         }
